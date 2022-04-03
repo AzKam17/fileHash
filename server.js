f854f9fb401f544d4cbe427ddaf1b897bb61d606
AzKam <azizkamadou17@gmail.com>
2022-04-03 08:26:52 +0000

    Final version

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const path = require('path');
 const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const fs = require('fs');
 const app = express();
 const axios = require('axios');
+const API = process.env.API || 'http://localhost:9986/';
 
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
+app.use(express.static('./public'));
 
-//Génération des clés
+let clesParNavigateur = {};
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
-const key = ec.genKeyPair();
-const clePublique = key.getPublic().encode('hex');
 
 // default options
 app.use(fileUpload());
+app.use(cookieParser());
+
+const getKeys = (cookie) => {
+    //Génération des clés
+    let key = ec.genKeyPair();
+    return [key, key.getPublic().encode('hex')];
+}
+
+const randomStr = (length) => {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
 
 app.post('/upload', function(req, res) {
     let sampleFile;
@@ -41,15 +60,15 @@ app.post('/upload', function(req, res) {
         hashSum.update(fileBuffer);
         const hex = hashSum.digest('hex');
 
-        const signature = key.sign(hex);
+        const signature = clesParNavigateur[req.cookies.keys][0].sign(hex);
         const der = signature.toDER("hex");
 
         axios({
             method: 'post',
-            url: 'http://localhost:9986/transaction/new',
+            url: API + '/transaction/new',
             data: {
                 "from":"046d0d88463a46c80faa88b976b60498df78ea48ffacb62d17bb98518266c175bbc073a31bcd0c80e7099272d92490a032757cb3960b9502caaa3d8f1a22a0f76a",
-                "to": "" + clePublique,
+                "to": "" + clesParNavigateur[req.cookies.keys][1],
                 "fileHash": "" + der,
                 "amount":100
             }
@@ -61,15 +80,27 @@ app.post('/upload', function(req, res) {
 
         fs.unlinkSync(uploadPath);
 
-        res.send('Fichier inscrit sur la chain, votre clé publique est : ' + clePublique);
+        res.render('sended', {
+            clePub: clesParNavigateur[req.cookies.keys][1]
+        });
     });
 });
 
-
 const port = process.env.PORT || 8080;
 
-app.get('/upload', function(req, res) {
-    res.render('index')
+app.get('/', function(req, res) {
+    let cookies = req.cookies.keys;
+    if(cookies === undefined || clesParNavigateur[cookies] === undefined){
+        console.log("Pas de clés, on génère de nouvelles clés");
+        cookies = randomStr(15);
+        res.cookie("keys", cookies);
+        clesParNavigateur['' + cookies] = getKeys(cookies);
+        console.log(getKeys(cookies));
+    }
+
+    res.render('index', {
+        clePub: clesParNavigateur[cookies][1]
+    })
 });
 
 app.get('/check', function(req, res){
@@ -79,11 +110,11 @@ app.get('/check', function(req, res){
 app.post('/verify', (req, res) => {
 
     let blocks = [];
-    axios.get('http://localhost:9986/')
+    axios.get(API)
         .then((response) => {
             const chain = response.data;
             chain.map(x => {
-                if(x['data'][0]['to']===clePublique) {
+                if(x['data'][0]['to']=== req.body.clePublique) {
                     blocks.push(x['data'][0])
                 }
             });
@@ -124,10 +155,23 @@ app.post('/verify', (req, res) => {
             });
 
             fs.unlinkSync(uploadPath);
-            res.send(result ? 'Oui' : 'Non');
+            res.render('result', {
+                msg: result ? " a bien été émis par le propriétaire de la clé publique fournie" : ' ne provient pas du propriétaire de la clé publique'
+            });
         });
     });
 })
+
+app.get('/blockchain', (req, res) => {
+    axios.get(API)
+        .then((response) => {
+            const chain = response.data;
+            console.log(chain[1].data)
+            res.render('blockchain', {
+                blocks: chain
+            });
+        });
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
